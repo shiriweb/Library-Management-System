a5925448a0870a94de960770a115b68092a4d16a
Shirisha Singh <shirisha.singh36@gmail.com>
2026-09-05 14:12:22 +0545

    feat: created custom user model with role field

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT =  "STUDENT", 'Student'
+        LIBRARIAN = "LIBRARIAN" , 'Librarian'
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices= Role.choices, default=Role.STUDENT)
+
+    def __str__(self):
+        return {self.username} - {self.role}
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,3 +125,4 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+AUTH_USER_MODEL = "accounts.User"
